0b00df38ee5486aebe1922245d1b784faa177efd
Maxim <maxim.gorbatyuk@outlook.com>
2020-11-11 15:20:10 +0600

    Have added scripts

--- scripts/database_run.ps1
+++
@@ -1,3 +0,0 @@
-docker-compose -f "postgresql/docker-compose.yml" stop
-docker-compose -f "postgresql/docker-compose.yml" rm --force
-docker-compose -f "postgresql/docker-compose.yml" up --build database pgadmin4
